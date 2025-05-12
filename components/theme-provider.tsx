0dfc160a2d6063c40a785834fe39dc931f431262
SimonTechle <95485606+SimonTechle@users.noreply.github.com>
2025-05-12 22:14:47 +0200

    Update theme-provider.tsx
    corrected merge

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use client"
 
 import * as React from "react"
@@ -8,16 +7,3 @@ import { type ThemeProviderProps } from "next-themes"
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
-=======
-'use client'
-
-import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
->>>>>>> a71d26b7cd6ca4df9d5f8e3a012e25834d465c84
